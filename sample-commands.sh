6b3b83f08bbf67ddc3dc364975f4ec5d634888e0
Ignacio Rodriguez <inz@fastmicroservices.com>
2019-05-10 10:39:39 +0200

    sample commands to help in testing

---
+++ sample-commands.sh
@@ -0,0 +1,8 @@
+curl -d '{"type":"npm", "url":"cd:/npm/npmjs/-/redie/0.3.0"}' -H "Content-Type: application/json" -H "X-token: secret" -X POST http://localhost:5000/requests
+curl -d '{"type":"npm", "url":"cd:/npm/npmjs/-/react-color/2.17.3"}' -H "Content-Type: application/json" -H "X-token: secret" -X POST http://localhost:5000/requests
+curl -d '{}' -H "Content-Type: application/json" -H "X-token: secret" -X POST http://localhost:4000/curations/sync
+
+curl -d '{"type":"npm", "url":"cd:/npm/npmjs/-/async/1.5.0"}' -H "Content-Type: application/json" -H "X-token: secret" -X POST http://localhost:5000/requests
+curl -d '{"type":"npm", "url":"cd:/npm/npmjs/-/async/1.5.0"}' -H "Content-Type: application/json" -H "X-token: secret" -X POST http://localhost:5000/requests
+
+curl -d '[{"tool":"package","coordinates":"npm/npmjs/-/async/3.0.1-0"}]' -H "Content-Type: application/json" -H "X-token: secret" -X POST http://localhost:4000/harvest
