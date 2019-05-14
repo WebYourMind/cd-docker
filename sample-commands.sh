86ac3d8989f5ad9de96f0d941b8ed175ab624997
Ignacio Rodriguez <inz@fastmicroservices.com>
2019-05-14 10:33:11 +0200

    adding azurite

--- sample-commands.sh
+++ sample-commands.sh
@@ -6,3 +6,7 @@ curl -d '{"type":"npm", "url":"cd:/npm/npmjs/-/async/1.5.0"}' -H "Content-Type: 
 curl -d '{"type":"npm", "url":"cd:/npm/npmjs/-/async/1.5.0"}' -H "Content-Type: application/json" -H "X-token: secret" -X POST http://localhost:5000/requests
 
 curl -d '[{"tool":"package","coordinates":"npm/npmjs/-/async/3.0.1-0"}]' -H "Content-Type: application/json" -H "X-token: secret" -X POST http://localhost:4000/harvest
+
+
+curl -d '[{"tool":"package","coordinates":"npm/npmjs/-/async/3.0.1-0"}]' -H "Content-Type: application/json" -H "X-token: secret" -X POST http://localhost:4000/harvest
+curl -d '[{"tool":"package","coordinates":"npm/npmjs/-/electron/2.0.1"},{"tool":"package","coordinates":"npm/npmjs/-/electron/2.0.11"}]' -H "Content-Type: application/json" -H "X-token: secret" -X POST http://localhost:4000/harvest
